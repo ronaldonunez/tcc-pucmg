4baa4ec4734916173facdcf796b8dba44b67c3de
Ronaldo Nunez <ronaldo.viera@gmail.com>
2017-09-13 17:57:50 -0300

    Implementado mostrar as notas no PDF.
    Signed-off-by: Ronaldo Nunez <ronaldo.viera@gmail.com>

--- files/serv-gera-historico.py
+++ files/serv-gera-historico.py
@@ -9,16 +9,21 @@ def genera_pdf(aluno, historico):
     canva.setLineWidth(.3)
     canva.setFont('Helvetica', 12)
      
-    canva.drawString(30,750, 'Aluno:')
-    canva.drawString(80,750, aluno['nome'])
-    canva.drawString(30,735, 'Curso:')
-    canva.drawString(80,735, aluno['curso'])
-    canva.line(30,730,580,730)
+    canva.drawString(30, 750, 'Aluno:')
+    canva.drawString(80, 750, aluno['nome'])
+    canva.drawString(30, 735, 'Curso:')
+    canva.drawString(80, 735, aluno['curso'])
+    canva.line(30, 730, 580, 730)
     
     coord_y = 713 
-    canva.drawString(30,718,'Disciplina')
-    canva.drawString(550,718,'Nota')
-    canva.line(30,coord_y,580,coord_y)
+    canva.drawString(30, 718,'Disciplina')
+    canva.drawString(550, 718,'Nota')
+    canva.line(30, coord_y, 580, coord_y)
+
+    for hist in historico:
+        coord_y = coord_y - 20
+        canva.drawString(30, coord_y, hist['disciplina'])
+        canva.drawString(555, coord_y, str(hist['nota']))
 
     return canva.getpdfdata()
 
